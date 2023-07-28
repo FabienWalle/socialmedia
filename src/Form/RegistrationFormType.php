f3ab41ef2901522604ff6bbb4948ad3320632739
FabienWalle <fabienwalle13@gmail.com>
2023-07-28 18:19:58 +0200

    ça avance un peu

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,7 +6,9 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,36 +20,46 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
+            ->add('name',TextType::class, [
+                'attr' => [
+                    'class' => 'form-control my-2'
+                ]])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control my-2'
+                ]])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class'=>'form-control my-2'
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
                         'min' => 4,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit comporter au minimum {{limit}} caractères',
                         // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'max' => 180,
+                        'maxMessage'=>'Votre mot de passe ne doit pas comporter plus de {{limit}} caractères'
                     ]),
                 ],
             ])
-            ->add('bio')
+            ->add('bio',TextType::class, [
+        'attr' => [
+            'class' => 'form-control my-2'
+        ],
+                'required' => false])
             ->add('dob', DateType::class, [
-                'widget' => 'single_text'
-            ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control my-2'
                 ],
+                'required' => false
             ])
         ;
     }
